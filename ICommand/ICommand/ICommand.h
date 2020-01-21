60290dcda9f20b8fecac4bef67c3d3a3add13c1c
Michael-Brodsky <56896035+Michael-Brodsky@users.noreply.github.com>
2020-01-21 11:06:06 -0700

    Update ICommand.h

--- ICommand/ICommand/ICommand.h
+++ ICommand/ICommand/ICommand.h
@@ -13,17 +13,17 @@
 
 /*	***************************************************************************
  *
- *  This file is part of "Pretty Good" (Pg). "Pg" is free software: 
+ *  	This file is part of "Pretty Good" (Pg). "Pg" is free software: 
  *	you can redistribute it and/or modify it under the terms of the 
  *	GNU General Public License as published by the Free Software Foundation, 
  *	either version 3 of the License, or (at your option) any later version.
  *
- *  This file is distributed in the hope that it will be useful, but 
+ *  	This file is distributed in the hope that it will be useful, but 
  *	WITHOUT ANY WARRANTY; without even the implied warranty of 
  *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
  *	GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License 
+ *  	You should have received a copy of the GNU General Public License 
  *	along with this file. If not, see <http://www.gnu.org/licenses/>.
  *
  *	**************************************************************************/
@@ -49,15 +49,15 @@
  *	Several useful concrete types are also defined:
  *
  *		NullCommand:	a command object with an empty `execute()' method, 
- *						useful in cases where a function argument of type 
- *						`ICommand' cannot be NULL, such as when the function 
- *						takes a reference rather than a pointer to `ICommand'.
+ *				useful in cases where a function argument of type 
+ *				`ICommand' cannot be NULL, such as when the function 
+ *				takes a reference rather than a pointer to `ICommand'.
  *
- *		Command:		The `Command' types are templated callback types that 
- *						allow users to customize the callback signature with 
- *						template parameters. They can be templated as memeber 
- *						method or free-standing function callbacks, with or 
- *						without arguments (see <Callback.h>).
+ *		Command:	The `Command' types are templated callback types that 
+ *				allow users to customize the callback signature with 
+ *				template parameters. They can be templated as memeber 
+ *				method or free-standing function callbacks, with or 
+ *				without arguments (see <Callback.h>).
  *
  *	All of the concrete types defined here derive from `ICommand', which is 
  *	non-templated and can thus be placed into a collection of type `ICommand'.
@@ -86,7 +86,7 @@
 #if !defined ICOMMAND_H__
 #define ICOMMAND_H__ 20200118L
 
-#include <utils.h>		// _typename macro.
+#include <utils.h>	// _typename macro.
 #include <Callback.h>	// Callback types.
 
 struct ICommand	// "Command" design pattern abstract interface class.
@@ -121,7 +121,7 @@ private:
 };
 
 template <class Ret, class Arg>
-class Command<Ret, void, Arg> : public ICommand	// Command type for free-standing function callbacks with arguments.
+class Command<Ret, void, Arg> : public ICommand		// Command type for free-standing function callbacks with arguments.
 {
 	typedef typename Callback<Ret, void, Arg>::callback_t callback_type;
 public:
@@ -136,7 +136,7 @@ private:
 };
 
 template <class Ret, class Obj>
-class Command<Ret, Obj, void> : public ICommand	// Command type for member function callbacks without arguments.
+class Command<Ret, Obj, void> : public ICommand		// Command type for member function callbacks without arguments.
 {
 	typedef typename Callback<Ret, Obj, void>::callback_t callback_type;
 public:
