227af65532b3ab5039e0feb31cafb8350ce411af
Michael-Brodsky <56896035+Michael-Brodsky@users.noreply.github.com>
2020-01-24 10:02:35 -0700

    Update ICloneable.h

--- ICloneable/ICloneable.h
+++ ICloneable/ICloneable.h
@@ -1,1 +1,17 @@
+/*
+ *  This file defines an abstract interface class for creating/cloning 
+ *  objects using the C++ "Virtual Constructor Idiom".
+ */
 
+#if !defined ICLONEABLE_H__
+#define ICLONEABLE_H__ 20200124L
+
+#endif // !defined ICLONEABLE_H__
+
+struct ICloneable 
+{
+    virtual ~ICloneable() { }
+    
+    virtual ICloneable* clone() const = 0;  // Uses the copy constructor
+    virtual ICloneable* create() const = 0; // Uses the default constructor
+};
