808e3e7f0bc147803f49b9979ee49aa0741a4e1d
Michael-Brodsky <56896035+Michael-Brodsky@users.noreply.github.com>
2020-01-30 11:21:51 -0700

    Add files via upload

---
+++ Timers/IntervalTimer.cpp
@@ -0,0 +1,61 @@
+#include "IntervalTimer.h"
+
+IntervalTimer::IntervalTimer(Interval& interval, bool start_timer) :
+	IClockable(), interval_(interval), running_(), resume_(), time_()
+{
+	if (start_timer)
+		start();
+}
+
+void IntervalTimer::interval(Interval& intvl, bool rst = false)
+{
+	interval_ = intvl;
+	if (rst)
+		reset();
+}
+
+msecs_t IntervalTimer::elapsed() const
+{
+	return millis() - time_;
+}
+
+void IntervalTimer::start()
+{
+	msecs_t now = millis();
+
+	if (resume_)
+		time_ = now - interval_.interval_ + time_;
+	else
+		reset();
+	resume_ = false;
+	running_ = true;
+}
+
+void IntervalTimer::stop()
+{
+	running_ = false;
+	resume_ = true;
+	time_ = elapsed();
+}
+
+void IntervalTimer::reset()
+{
+	if (running_)
+		time_ = millis();
+	else
+		resume_ = false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+void IntervalTimer::clock() 
+{
+	if (running_ && !(elapsed() < interval_.interval_))
+		trigger();
+}
+
+void IntervalTimer::trigger()
+{
+	reset();
+	interval_.command_.execute();
+}
