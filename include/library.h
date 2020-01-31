7ce980a97b6dd89a2ca87e0a7b29010f5d1bab65
Michael-Brodsky <56896035+Michael-Brodsky@users.noreply.github.com>
2020-01-31 13:57:56 -0700

    Update library.h

--- include/library.h
+++ include/library.h
@@ -1,1 +1,17 @@
+/*
+ *  This file #includes library dependencies.
+ */
+#if !defined LIBRARY_H__
+# define LIBRARY_H__ 20200131L
 
+# if defined ARDUINO
+#  if ARDUINO >= 100 
+#   include "Arduino.h"
+#  else
+#   include "WProgram.h"
+#  endif // ARDUINO >= 100 
+# endif // defined ARDUINO
+
+# include "types.h"
+
+#endif // !defined LIBRARY_H__
